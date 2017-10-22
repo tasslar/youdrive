1ba6ded9c1f8a0665fbdbeb00314851f0e65d2e5
vigneshvickey <vigneshvickey0@gmail.com>
2017-10-22 16:24:47 +0530

    new pages

--- vignesh/js/main.js
+++ vignesh/js/main.js
@@ -2,4 +2,7 @@ $(document).ready(function(){
     $("#hide").click(function(){
         $("#show").slideToggle("slow");
     });
+     $("#show_form").click(function(){
+        $("#hide_form").slideToggle("slow");
+    });
 });
