7ec1df9dc15f5f81c6f5eac014dab4d1c49bde91
sreenivasan <sreenivasan@dexeldesigns.com>
2017-10-26 11:16:56 +0530

    filechanges

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,8 @@
 
 Route::get('/info',function(){ phpinfo(); });
 Route::get('/', function () {
-    phpinfo();
-    // return redirect('login');
+    //phpinfo();
+     return redirect('login');
 });
 
 Route::get('manage_branches', [
