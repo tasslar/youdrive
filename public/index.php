a0efe4f199c53da104984d9763a6d502efdfed08
sreenivasan <sreenivasan@dexeldesigns.com>
2017-10-26 10:20:56 +0530

    add htdocs files

--- public/index.php
+++ public/index.php
@@ -7,7 +7,6 @@
  * @author   Taylor Otwell <taylor@laravel.com>
  */
 
-define('LARAVEL_START', microtime(true));
 
 /*
 |--------------------------------------------------------------------------
@@ -21,7 +20,7 @@ define('LARAVEL_START', microtime(true));
 |
 */
 
-require __DIR__.'/../vendor/autoload.php';
+require __DIR__.'/../bootstrap/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
