0434df343f111ae60fb6bff0ee5673b71c9a473b
sreenivasan <sreenivasan057@gmail.com>
2017-10-26 21:00:22 +0530

    add htaccess files

--- public/index.php
+++ public/index.php
@@ -1,12 +1,9 @@
 <?php
-
 /**
- * Laravel - A PHP Framework For Web Artisans
+ * Laravel - A PHP Framework For Web Artisans.
  *
- * @package  Laravel
- * @author   Taylor Otwell <taylor@laravel.com>
+ * @author   Taylor Otwell <taylorotwell@gmail.com>
  */
-
 
 /*
 |--------------------------------------------------------------------------
@@ -16,11 +13,11 @@
 | Composer provides a convenient, automatically generated class loader for
 | our application. We just need to utilize it! We'll simply require it
 | into the script here so that we don't have to worry about manual
-| loading any of our classes later on. It feels great to relax.
+| loading any of our classes later on. It feels nice to relax.
 |
 */
 
-require __DIR__.'/bootstrap/autoload.php';
+require __DIR__.'/../bootstrap/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -34,21 +31,21 @@ require __DIR__.'/bootstrap/autoload.php';
 |
 */
 
-$app = require_once __DIR__.'/bootstrap/app.php';
+$app = require_once __DIR__.'/../bootstrap/app.php';
 
 /*
 |--------------------------------------------------------------------------
 | Run The Application
 |--------------------------------------------------------------------------
 |
-| Once we have the application, we can handle the incoming request
-| through the kernel, and send the associated response back to
+| Once we have the application, we can simply call the run method,
+| which will execute the request and send the response back to
 | the client's browser allowing them to enjoy the creative
 | and wonderful application we have prepared for them.
 |
 */
 
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+$kernel = $app->make('Illuminate\Contracts\Http\Kernel');
 
 $response = $kernel->handle(
     $request = Illuminate\Http\Request::capture()
